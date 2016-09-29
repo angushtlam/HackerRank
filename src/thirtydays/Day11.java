4a7418719b73e08a4b94672d5cbd530ad7396790
Angus Lam <angushtlam@gmail.com>
2016-09-29 14:45:12 -0400

    Day 11

--- src/thirtydays/Day11.java
+++ src/thirtydays/Day11.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 /**
  * Created by angus on 9/29/16.
+ * Day 11: 2D Arrays
  */
 public class Day11 {
 	public static void main(String[] args) {
@@ -16,10 +17,25 @@ public class Day11 {
 			}
 		}
 
-		// Search only in possible locations.
+		int largestHourglass = -9 * 16;
 
+		// Start searching from the center piece.
+		for (int locX = 1; locX < 5; locX++) {
+			for (int locY = 1; locY < 5; locY++) {
+				int total = 0;
+				total += arr[locX][locY];
+				total += arr[locX - 1][locY - 1];
+				total += arr[locX - 1][locY    ];
+				total += arr[locX - 1][locY + 1];
+				total += arr[locX + 1][locY - 1];
+				total += arr[locX + 1][locY    ];
+				total += arr[locX + 1][locY + 1];
 
+				if (total > largestHourglass) largestHourglass = total;
+			}
+		}
 
+		System.out.println(largestHourglass);
 		in.close();
 	}
 }
